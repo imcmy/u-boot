dfddddb7e20418ced23c9b668a499d650c3289e9
Mingyi Chen <mingyi.chen@uci.edu>
2024-04-10 15:12:23 -0700

    [Update]

---
+++ board/custom/rocket-chip-vpk120/rocket-chip-vpk120.c
@@ -0,0 +1,9 @@
+int board_init(void)
+{
+	return 0;
+}
+
+int board_late_init(void)
+{
+	return 0;
+}
---
+++ include/configs/rocket-chip-vpk120.h
@@ -0,0 +1,19 @@
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <linux/sizes.h>
+
+#define CONFIG_SYS_SDRAM_BASE		0x80000000
+#define CONFIG_STANDALONE_LOAD_ADDR	0x80000000
+
+#define RISCV_MMODE_TIMERBASE		0x2000000
+#define RISCV_MMODE_TIMER_FREQ		1000000
+
+#define CONFIG_ENV_IS_NOWHERE   1 
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+    "autoload=no\0"
+
+#define CONFIG_TIMESTAMP
+
+#endif /* __CONFIG_H */
