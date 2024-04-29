d7fad568f0d9070f237b50b3588e95029608d5b9
Mingyi Chen <mingyi.chen@uci.edu>
2024-04-29 10:26:07 -0700

    [Update]

--- board/custom/rocket-chip-vpk120/rocket-chip-vpk120.c
+++ board/custom/rocket-chip-vpk120/rocket-chip-vpk120.c
@@ -1,3 +1,8 @@
+#include <common.h>
+#include <asm/global_data.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
 int board_init(void)
 {
 	return 0;
@@ -6,4 +11,16 @@ int board_init(void)
 int board_late_init(void)
 {
 	return 0;
+}
+
+void *board_fdt_blob_setup(int *err)
+{
+	void *fdt_blob = (void *)0x800030000;
+
+    *err = 0;
+    if (fdt_magic(fdt_blob) == FDT_MAGIC)
+		return fdt_blob;
+
+    *err = -EINVAL;
+    return NULL;
 }
--- include/configs/rocket-chip-vpk120.h
+++ include/configs/rocket-chip-vpk120.h
@@ -3,8 +3,7 @@
 
 #include <linux/sizes.h>
 
-#define CONFIG_SYS_SDRAM_BASE		0x80000000
-#define CONFIG_STANDALONE_LOAD_ADDR	0x80000000
+#define CONFIG_SYS_SDRAM_BASE		0x800000000
 
 #define RISCV_MMODE_TIMERBASE		0x2000000
 #define RISCV_MMODE_TIMER_FREQ		1000000
@@ -13,6 +12,7 @@
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
     "autoload=no\0"
+    // "boot_kernel=tftpboot 0x800100000 10.0.0.1:image.itb;bootm 0x800100000\0"
 
 #define CONFIG_TIMESTAMP
 
